094f9828b94d6b6c38332a11e68370fc91d760cf
bharatrai53 <bharatrai2016@gmail.com>
2023-01-20 14:01:37 -0800

    added public folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const hoePageRoutes = require('./homePageRoutes');
+const homePageRoutes = require('./homePageRoutes');
 
 router.use('/users', userRoutes);
 router.use('/homePage', homePageRoutes);
