fa9bebbaea428e73c55f8d553115086567d9faeb
Casey Dwyer <caseyjdwyer@gmail.com>
2023-01-23 14:56:15 -0800

    Get posts to render on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,43 +7,29 @@ router.get("/", withAuth, async (req, res) => {
   try {
     // --- POST DATA ---
     // Get posts and comments with user data
-    // const postData = await Post.findAll({
-    //   include: [
-    //     {
-    //       model: User,
-    //       attributes: ["name"],
-    //     },
-    //     {
-    //       model: Comment,
-    //       include: { User },
-    //     },
-    //   ],
-    // });
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Comment,
+          include: User,
+        },
+      ],
+    });
 
     // Serialize post data so template can read it
-    // const posts = postData.map((post) => post.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-    // -- COMMENT DATA --
-    // Get post comment data and join user data
-    // const commentData = await Comment.findAll({
-    //   include: [
-    //     {
-    //       model: User,
-    //       attributes: ["name"],
-    //     },
-    //   ],
-    // });
-    // // Serialize comment data so template can read it
-    // const comments = commentData.map((comment) => comment.get({ plain: true }));
-    // console.log(comments);
+    console.log(posts);
 
-
-    // console.log(posts);
-    
     // -- RENDER HOMEPAGE --
     // Pass serialized data and session flag into template and render Homepage
-    res.render("homepage");
+    res.render("homepage", { posts });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
